ada38b839508a72aa88833e9a729d8797dbbb810
kedarnathyadav <dkny097@gmail.com>
2024-04-16 19:42:33 +0100

    finished calculator using console

--- Calculator/src/calci/Calculator.java
+++ Calculator/src/calci/Calculator.java
@@ -1,11 +1,62 @@
+		
+		
+
 package calci;
+
+import java.util.Scanner;
 
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+
+
+		        Scanner scanner = new Scanner(System.in);
+
+		        System.out.println("Enter first number:");
+		        double num1 = scanner.nextDouble();
+
+		        System.out.println("Enter second number:");
+		        double num2 = scanner.nextDouble();
+
+		        System.out.println("Enter an operator (+, -, *, /):");
+		        char operator = scanner.next().charAt(0);
+
+		        scanner.close();
+		        double output;
+
+		        switch(operator)
+		        {
+		            case '+':
+		                output = num1 + num2;
+		                break;
+
+		            case '-':
+		                output = num1 - num2;
+		                break;
+
+		            case '*':
+		                output = num1 * num2;
+		                break;
+
+		            case '/':
+		                if (num2 != 0) {
+		                    output = num1 / num2;
+		                } else {
+		                    System.out.println("Error! Dividing by zero is not allowed.");
+		                    return;
+		                }
+		                break;
+
+		            default:
+		                System.out.println("You have entered wrong operator");
+		                return;
+		        }
+
+		        System.out.println(num1+" "+operator+" "+num2+": "+output);
+		    }
+
+
 
 	}
 
-}
+
